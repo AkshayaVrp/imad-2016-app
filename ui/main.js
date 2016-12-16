eb37e304f5a3f5bd5acdfd141a4b19d495837e95
AkshayaVrp <rvrakshaya@gmail.com>
2016-12-16 20:08:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,16 +2,23 @@
 var button=document.getElementById('counter');
 
 button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-    if(request.readyState===XMLHttpRequest.DONE){
-        if(request.status===200){
-            var counters=request.responseText;
-           var span=document.getElementById('count');
-          span.innerHTML=counters.toString();}
-     }
     
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+    if(request.readyState === XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+            var counter=request.responseText;
+           var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+            
+        }
+    }
+    
+    };
     request.open('GET','http://akshayavrp.imad.hasura-app.io/counter',true);
     request.send(null);
-    };
+    
 };
