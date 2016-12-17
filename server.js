2137905ed4b3e95d1df275e32a4c19b72197d612
AkshayaVrp <rvrakshaya@gmail.com>
2016-12-17 16:36:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config={
  user:'akshayavrp',
@@ -15,6 +16,8 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+//to tell our express app to tell that if it sees json contetn load that into req.body variable
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -29,6 +32,23 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res) {
     var hashedString=hash(req.params.input,'random-string');
     res.send(hashedString);
+    
+});
+
+app.post('/create-user',function(req,res) {
+    
+    var username=req.body.username;
+    var password=req.body.password;//assume username and password are JSON request
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }else{
+           res.send('User succesfully created!!'+username);
+       }
+    });
     
 });
 
